fb4bb1afa6249581cb0853e992de00db306f5e83
jeanquark <jm.kleger@gmail.com>
2016-06-20 12:43:02 +0200

    A bit of cleanup

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -65,17 +65,17 @@ class ImageController extends Controller
                     $original_height = $size[1];
                     $original_ratio = $original_width/$original_height; // Get original ratio width/height
 					
-                    if ($original_ratio > $new_ratio) { // If ratio is greater than optimal (900px/300px)
+                    if ($original_ratio > $new_ratio) { // If ratio is greater than optimal
                         $width = $original_width/($original_height/$new_height);
                         $height = $new_height;
                         $shift_x = (($width - $new_width)*($original_width/$width))/2;
                         $shift_y = 0;
-                    } elseif ($original_ratio < $new_ratio) { // If ratio is less than optimal (900px/300px)
+                    } elseif ($original_ratio < $new_ratio) { // If ratio is lesser than optimal
                         $width = $new_width;
                         $height = $original_height/($original_width/$new_width);
                         $shift_x = 0;
-                        $shift_y = (($height - $new_height)*($original_height/$height))/2; //should be equal to 330 or 220
-                    } else { // If ratio is already optimal (900px/300px = 3)
+                        $shift_y = (($height - $new_height)*($original_height/$height))/2;
+                    } else { // If ratio is already optimal
                         $width = $new_width;
                         $height = $new_height;
                         $shift_x = 0; // No need to crop horizontally
@@ -83,11 +83,7 @@ class ImageController extends Controller
                     }
 
                     $src = imagecreatefromstring(file_get_contents($image_path));
-
-                    // And erase the original image
-                    // With crop
                     $dst = imagecreatetruecolor($new_width,$new_height);
-                    //imagecopyresampled($dst, $src, 0, 0, $shift_x, $shift_y, $new_width, $new_height, $size[0], $size[1]);
                     imagecopyresampled($dst, $src, 0, 0, $shift_x, $shift_y, $width, $height, $original_width, $original_height);
                     imagedestroy($src); // Free up memory
                     imagejpeg($dst, $image_path, 100); // adjust format as needed
